0d40391ea638f77b9949fe07797481fc907855e1
BOZ2323 <lillibaaske@gmx.de>
2017-11-02 12:01:53 +0100

    change database module to connect with different data.jsons.

--- homework/week_2_mywork/Data/database.js
+++ homework/week_2_mywork/Data/database.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 
-
-exports.save = (guests) => {
-    fs.writeFileSync('./Data/data.json', JSON.stringify(guests))
+const chooseDataJson = {
+    DinnerPartyguest: {
+        name: 'DinnerPartyGuest',
+        file: './Data/Dinnerguest_data.json'
+    },
+    Recipe: {
+        name: 'Recipe',
+        file: './Data/Recipe_data.json'
+    },
 }
-exports.load = () => {
-    return JSON.parse(fs.readFileSync('./Data/data.json', 'utf8'))
+exports.save = (database, item) => {
+    const name = chooseDataJson[database].name
+    const file = chooseDataJson[database].file
+
+    fs.writeFileSync(file, JSON.stringify({
+        [name]: item
+    }))
+}
+exports.load = (database) => {
+    const file = chooseDataJson[database].file
+    return JSON.parse(fs.readFileSync(file, 'utf8'))
 }
